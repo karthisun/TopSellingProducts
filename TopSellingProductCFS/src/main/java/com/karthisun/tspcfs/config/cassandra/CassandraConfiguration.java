ad9290a3843a075ed85f362086bd53fca0f15531
K Karthikeyan <karthi.sun@gmail.com>
2019-07-04 21:34:40 +0530

    softcodeing the properties

--- TopSellingProductCFS/src/main/java/com/karthisun/tspcfs/config/cassandra/CassandraConfiguration.java
+++ TopSellingProductCFS/src/main/java/com/karthisun/tspcfs/config/cassandra/CassandraConfiguration.java
@@ -1,6 +1,7 @@
 package com.karthisun.tspcfs.config.cassandra;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
@@ -11,26 +12,68 @@ import org.springframework.data.cassandra.core.mapping.CassandraMappingContext;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
 @Configuration
+@ConfigurationProperties
 @EnableCassandraRepositories(basePackages = "com.karthisun.tspcfs.cassandra.repository")
 public class CassandraConfiguration extends AbstractCassandraConfiguration {
 
+	
 
+	@Value("${spring.data.cassandra.keyspace-name}")
+	private String keyspace;
 
+	@Value("${spring.data.cassandra.contact-points}")
+	private String contactPoints;
+	
+	@Value("${spring.data.cassandra.username}")
+	private String username;
+	
+	@Value("${spring.data.cassandra.password}")
+	private String password;
+	
+	@Value("${spring.data.cassandra.dataCenter}")
+	private String dataCenter;
+	
+	@Value("${spring.data.cassandra.port}")
+	private int port;
 
 	  @Override
 	  protected String getKeyspaceName() {
-	    return "products";
+	    return keyspace;
 	  }
 
 	  @Override
 	  protected String getContactPoints() {
-	    return "localhost";
+	    return contactPoints;
 	  }
 
 	  @Override
 	  protected int getPort() {
-	    return 9042;
+	    return port;
 	  }
+	  
+	  public String getUsername() {
+			return username;
+		}
+
+		public void setUsername(String username) {
+			this.username = username;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+
+		public String getDataCenter() {
+			return dataCenter;
+		}
+
+		public void setDataCenter(String dataCenter) {
+			this.dataCenter = dataCenter;
+		}
 
 	  @Override
 	  public SchemaAction getSchemaAction() {
@@ -43,10 +86,4 @@ public class CassandraConfiguration extends AbstractCassandraConfiguration {
 					"com.karthisun.tspcfs.cassandra.model"
 			};
 		}
-
-	/*
-	 * @Override protected String getKeyspaceName() { // TODO Auto-generated method
-	 * stub return "products"; }
-	 */
-	
 }
